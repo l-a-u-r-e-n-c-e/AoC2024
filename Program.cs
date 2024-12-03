2d63bc64c495e492b9ede871f9149f32c07f1c35
Laurence Alldis <laurence.alldis@bdo.co.uk>
2024-12-03 13:36:37 +0000

    Added program setup

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+using AoC2024.days;
+using Microsoft.Extensions.Configuration;
+
+// Setup
+string environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Production";
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+    .Build();
+var inputBaseFilePath = configuration["inputFileBasePath"];
+
