94e31868f65c13d051487e0017ff68c921f07e9d
Laurence Alldis <laurence.alldis@bdo.co.uk>
2024-12-03 17:55:18 +0000

    Added private method for processing input

--- days/Day1.cs
+++ days/Day1.cs
@@ -4,5 +4,24 @@ public class Day1 : Day
 {
     public Day1(string inputFilePath) : base(inputFilePath)
     {
+
+    private (List<int>, List<int>) processInput(string input)
+    {
+        var leftList = new List<int>();
+        var rightList = new List<int>();
+
+        var inputAsArray = Input.Replace("\n", "   ").Split("   ");
+
+        for(int i = 0; i < inputAsArray.Length; i += 2)
+        {
+            var left = int.Parse(inputAsArray[i]);
+            var right = int.Parse(inputAsArray[i+1]);
+            leftList.Add(left);
+            rightList.Add(right);
+        }
+
+        leftList.Sort();
+        rightList.Sort();
+        return (leftList, rightList);
     }
 }
