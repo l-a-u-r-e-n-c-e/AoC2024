c51cd2beb0344fdcd484974bec8b4d7d5068c1de
Laurence Alldis <laurence.alldis@bdo.co.uk>
2024-12-03 17:55:46 +0000

    Added part 1 logic

--- days/Day1.cs
+++ days/Day1.cs
@@ -2,8 +2,23 @@ namespace AoC2024.days;
 
 public class Day1 : Day
 {
+    private List<int> _leftList;
+    private List<int> _rightList;
     public Day1(string inputFilePath) : base(inputFilePath)
     {
+        (_leftList, _rightList) = processInput(Input);
+    }
+
+    public int Part1()
+    {
+        var totalDistance = 0;
+
+        for(int i = 0; i < _leftList.Count(); i++)
+        {
+            totalDistance += _leftList[i] > _rightList[i] ? _leftList[i] - _rightList[i] : _rightList[i] - _leftList[i];
+        }
+        return totalDistance;
+    }
 
     private (List<int>, List<int>) processInput(string input)
     {
