bef98da4c0e1e50c28abd20c4d2b177a4da1842e
Laurence Alldis <laurence.alldis@bdo.co.uk>
2024-12-04 23:44:35 +0000

    Added Day 2 Part 2 logic

--- days/Day2.cs
+++ days/Day2.cs
@@ -2,15 +2,16 @@ namespace AoC2024.days;
 
 public class Day2 : Day
 {
+    private string[][] _inputArrays;
     public Day2(string inputFilePath) : base(inputFilePath)
     {
+        _inputArrays = processInput(Input);
     }
 
     public int Part1()
     {
-        var inputArrays = processInput(Input);
         var safeReports = 0;
-        foreach (var report in inputArrays)
+        foreach (var report in _inputArrays)
         {
             safeReports += isSafe(report) ? 1 : 0;
         }
@@ -19,7 +20,13 @@ public class Day2 : Day
 
     public int Part2()
     {
-        return default;
+        var safeReports = 0;
+        foreach (var report in _inputArrays)
+        {
+            if (isSafe(report)) { safeReports++; continue; }
+            safeReports += isCorrectable(report) ? 1 : 0;
+        }
+        return safeReports;
     }
 
     private string[][] processInput(string input)
@@ -47,5 +54,16 @@ public class Day2 : Day
             if (difference < 1 || difference > 3) { return false; }
         }
         return true;
+    }
+
+    private bool isCorrectable(string[] report)
+    {
+        for(int i = 0; i < report.Length; i++)
+        {
+            var amendedReport = new List<string>(report);
+            amendedReport.RemoveAt(i);
+            if (isSafe(amendedReport.ToArray())) { return true; }
+        }
+        return false;
     }
 }
