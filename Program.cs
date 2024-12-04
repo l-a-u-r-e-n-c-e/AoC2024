feb994460b0da11a605b4bff440034213509dfe4
Laurence Alldis <laurence.alldis@bdo.co.uk>
2024-12-04 10:23:44 +0000

    Implemented Day 2 Part 1

--- Program.cs
+++ Program.cs
@@ -13,9 +13,14 @@ var inputBaseFilePath = configuration["inputFileBasePath"];
 
 // Day 1
 var day1 = new Day1($"{inputBaseFilePath}/{configuration["days:day1:inputFileName"]}");
-var part1Answer = day1.Part1();
-var part2Answer = day1.Part2();
+var d1p1Answer = day1.Part1();
+var d1p2Answer = day1.Part2();
 
-Console.WriteLine($"The answer to Day 1 Part 1 is {part1Answer} and the answer to Part 2 is {part2Answer}");
+Console.WriteLine($"The answer to Day 1 Part 1 is {d1p1Answer} and the answer to Part 2 is {d1p2Answer}");
 
 // Day 2
+var day2 = new Day2($"{inputBaseFilePath}/{configuration["days:day2:inputFileName"]}");
+var d2p1Answer = day2.Part1();
+var d2p2Answer = "not implemented yet.";
+
+Console.WriteLine($"The answer to Day 2 Part 1 is {d2p1Answer} and the answer to Part 2 is {d2p2Answer}");
