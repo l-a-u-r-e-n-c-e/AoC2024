a1af7c01615ca294a448895e5938b6c53bdd69d5
Laurence Alldis <laurence.alldis@bdo.co.uk>
2024-12-06 14:31:04 +0000

    Added Day 3 Part 1 logic

--- days/Day3.cs
+++ days/Day3.cs
@@ -8,6 +8,44 @@ public class Day3 : Day
 
     public int Part1()
     {
-        return default;
+        var result = 0;
+        for(int i = 0; i < Input.Length; i++)
+        {
+            var isMullPrefix = Input[i] == 109 // m
+                && Input[i+1] == 117 // u 
+                && Input[i+2] == 108 // l
+                && Input[i+3] == 40; // (
+            if (!isMullPrefix) { continue; }
+
+            var multiplicandLastIndex = getLastIndex(i, 4, ',');
+            if (multiplicandLastIndex == 0) { continue; }
+            var multiplicand = i+4 == multiplicandLastIndex 
+                ? Input[i+4] - '0' 
+                : int.Parse(Input[(i+4)..(multiplicandLastIndex+1)]);
+            
+            var multiplierLastIndex = getLastIndex(multiplicandLastIndex, 2, ')');
+            if (multiplierLastIndex == 0) { continue; }
+            var multiplier = multiplicandLastIndex+2 == multiplierLastIndex 
+                ? Input[multiplicandLastIndex+2] - '0' 
+                : int.Parse(Input[(multiplicandLastIndex+2)..(multiplierLastIndex+1)]);
+
+            result += multiplicand * multiplier;
+        }
+
+        return result;
     }
+     private int getLastIndex(int i, int offset, char validTerminator)
+     {
+        char terminator = new();
+        var isNumber = Input[i+offset] >= 48 && Input[i+offset] <= 57;
+        while(isNumber)
+        {
+            offset++;
+            isNumber = Input[i+offset] >= 48 && Input[i+offset] <= 57;
+            terminator = Input[i+offset];
+        }
+
+        if (terminator != validTerminator) { return 0; }
+        return i+offset-1;
+     }
 }
