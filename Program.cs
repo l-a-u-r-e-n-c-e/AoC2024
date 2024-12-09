83f57ac5f9a7544d6533b42e0c2e9374bb213a7f
Laurence Alldis <laurence.alldis@bdo.co.uk>
2024-12-09 15:08:06 +0000

    Added GenerateInputPath method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using AoC2024.days;
+using AoC2024.days;
 using Microsoft.Extensions.Configuration;
 
 // Setup
@@ -8,25 +8,24 @@ var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
     .Build();
-var inputBaseFilePath = configuration["inputFileBasePath"];
-
+InputHandler inputHandler = new(configuration);
 
 // Day 1
-var day1 = new Day1($"{inputBaseFilePath}/{configuration["days:day1:inputFileName"]}");
+var day1 = new Day1(inputHandler.GenerateInputPath(1));
 var d1p1Answer = day1.Part1();
 var d1p2Answer = day1.Part2();
 
 Console.WriteLine($"The answer to Day 1 Part 1 is {d1p1Answer} and the answer to Part 2 is {d1p2Answer}");
 
 // Day 2
-var day2 = new Day2($"{inputBaseFilePath}/{configuration["days:day2:inputFileName"]}");
+var day2 = new Day2(inputHandler.GenerateInputPath(2));
 var d2p1Answer = day2.Part1();
 var d2p2Answer = day2.Part2();
 
 Console.WriteLine($"The answer to Day 2 Part 1 is {d2p1Answer} and the answer to Part 2 is {d2p2Answer}");
 
 // Day 3
-var day3 = new Day3($"{inputBaseFilePath}/{configuration["days:day3:inputFileName"]}");
+var day3 = new Day3(inputHandler.GenerateInputPath(3));
 var d3p1Answer = day3.Part1();
 var d3p2Answer = day3.Part2();
 
--- utilities/InputHandler.cs
+++ utilities/InputHandler.cs
@@ -1,23 +1,39 @@
 using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace AoC2024.utilities;
 
 public class InputHandler
 {
-    public InputHandler()
+    IConfiguration _config;
+    string _baseFilePath;
+    public InputHandler(IConfiguration config)
     {
-
+        _config = config;
+        _baseFilePath = _config["inputFileBasePath"]!;
+        validatePath(_baseFilePath);
     }
 
     public static string GetInput(string filePath)
     {
-        if (!File.Exists(filePath))
-        {
-            throw new FileNotFoundException($"The file '{filePath}' was not found.");
-        }
-
+        validatePath(filePath);
         using var reader = new StreamReader(filePath); 
         return reader.ReadToEnd();
+    }
+
+    public string GenerateInputPath(int dayNumber)
+    {
+        var path = $"{_baseFilePath}/{_config[$"days:day{dayNumber}:inputFileName"]}";
+        validatePath(path);
+        return path;
+    }
+
+    private static void validatePath(string path)
+    {
+        if (!File.Exists(path))
+        {
+            throw new FileNotFoundException($"The path {path} does not exist");
+        }
     }
 
 }
